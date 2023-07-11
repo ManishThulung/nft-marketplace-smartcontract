7893135c33720a06caef5be9cc060fba09b667c6
ManishThulung <manish.thulung58@gmail.com>
2023-07-11 23:31:08 +0545

    buyItem and cancelListed function added

--- contracts/NftMarketPlace.sol
+++ contracts/NftMarketPlace.sol
@@ -6,10 +6,16 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 error NftMarketPlace_PriceMustBeAboveZero();
 error NftMarketPlace__NotApprovedForMarketPlace();
-error NftMarketPlace__AlreadyListed(address nftAddress, uint256 tokenId);
 error NftMarketPlace__NotOwner();
+error NftMarketPlace__AlreadyListed(address nftAddress, uint256 tokenId);
+error NftMarketPlace__NotListed(address nftAddress, uint256 tokenId);
+error NftMarketPlace__PriceNotMet(
+    address nftAddress,
+    uint256 tokenId,
+    uint256 price
+);
 
-contract NftMarketPlace {
+contract NftMarketPlace is ReentrancyGuard {
     struct Listing {
         uint256 price;
         address seller;
@@ -21,15 +27,42 @@ contract NftMarketPlace {
         uint256 indexed tokenId,
         uint256 price
     );
+    event ItemBought(
+        address indexed buyer,
+        address indexed nftAddress,
+        uint256 indexed tokenId,
+        uint256 price
+    );
+    event ItemCancelled(
+        address indexed seller,
+        address indexed nftAddress,
+        uint256 indexed tokenId
+    );
 
     // NFT contract address => tokenId => Listing
-    mapping(address => mapping(uint256 => Listing)) s_listings;
+    mapping(address => mapping(uint256 => Listing)) private s_listings;
+
+    // seller address -> amount earned
+    mapping(address => uint256) private s_proceeds;
 
     constructor() {}
 
     ///////////////////////
     ///   Modifiers
     //////////////////////
+    modifier isOwner(
+        address nftAddress,
+        uint256 tokenId,
+        address spender
+    ) {
+        IERC721 nft = IERC721(nftAddress);
+        address owner = nft.ownerOf(tokenId);
+        if (spender != owner) {
+            revert NftMarketPlace__NotOwner();
+        }
+        _;
+    }
+
     modifier notListed(
         address nftAddress,
         uint256 tokenId,
@@ -42,15 +75,10 @@ contract NftMarketPlace {
         _;
     }
 
-    modifier isOwner(
-        address nftAddress,
-        uint256 tokenId,
-        address spender
-    ) {
-        IERC721 nft = IERC721(nftAddress);
-        address owner = nft.ownerOf(tokenId);
-        if (spender != owner) {
-            revert NftMarketPlace__NotOwner();
+    modifier isListed(address nftAddress, uint256 tokenId) {
+        Listing memory listing = s_listings[nftAddress][tokenId];
+        if (listing.price <= 0) {
+            revert NftMarketPlace__NotListed(nftAddress, tokenId);
         }
         _;
     }
@@ -87,5 +115,53 @@ contract NftMarketPlace {
         }
         s_listings[nftAddress][tokenId] = Listing(price, msg.sender);
         emit ItemListed(msg.sender, nftAddress, tokenId, price);
+    }
+
+    function cancelListing(
+        address nftAddress,
+        uint256 tokenId
+    )
+        external
+        isOwner(nftAddress, tokenId, msg.sender)
+        isListed(nftAddress, tokenId)
+    {
+        delete (s_listings[nftAddress][tokenId]);
+        emit ItemCancelled(msg.sender, nftAddress, tokenId);
+    }
+
+    function buyItem(
+        address nftAddress,
+        uint256 tokenId
+    ) external payable isListed(nftAddress, tokenId) nonReentrant {
+        Listing memory listedItem = s_listings[nftAddress][tokenId];
+        if (msg.value < listedItem.price) {
+            revert NftMarketPlace__PriceNotMet(
+                nftAddress,
+                tokenId,
+                listedItem.price
+            );
+        }
+
+        /**
+         * we dont want to directly send the seller money or transform them instead
+         * we want them to withdraw money temselves
+         */
+
+        // to track how much money people have earned selling thier nft
+        s_proceeds[listedItem.seller] =
+            s_proceeds[listedItem.seller] +
+            msg.value;
+
+        // once nft is bought, it is removed from the sold items
+        delete (s_listings[nftAddress][tokenId]);
+
+        // transfer to the buyer
+        // It is always better to call the external functon at the end of the contract after updating all the sates
+        IERC721(nftAddress).safeTransferFrom(
+            listedItem.seller,
+            msg.sender,
+            tokenId
+        );
+        emit ItemBought(msg.sender, nftAddress, tokenId, listedItem.price);
     }
 }
